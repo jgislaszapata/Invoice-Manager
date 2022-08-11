ed95db9a0538892763725f602fda783d97f8997c
applespicy <d.oshinowo@yahoo.com>
2022-08-11 15:43:10 -0400

    api updated

--- routes/api/client-routes.js
+++ routes/api/client-routes.js
@@ -3,28 +3,84 @@ const router = require('express').Router();
 const { Client } = require('../../models');
 
 //find all clients from database
-router.get('/', (req, res) => {
-
+router.get('/', async(req, res) => {
+    try {
+        const clientData = await Client.findAll();
+        res.status(200).json(clientData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 //find client by id
-router.get('/:id', (req, res) => {
-
-});
+router.get('/:id',async (req, res) => {
+    try {
+        const clientData = await Product.findByPk(req.params.id, {
+         
+          include: []
+        });
+    
+        if (!clientData) {
+          res.status(404).json({ message: 'No Client found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(clientData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+  
 
 //create a new client 
-router.post('/', (req, res) => {
-
+router.post('/', async(req, res) => {
+    try {
+        const clientData = await Client.create(req.body);
+        res.status(200).json(clientData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 //update client informaiton
-router.put('/:id', (req, res) => {
 
-});
+    router.put('/:id', (req, res) => {
+     Client.update({
+        client_name:req.body.client_name,
+        client_email:req.body.client_email,
+        client_phone:req.body.client_phone,
+     },
+     {
+        where:{
+            client_id:req.params.client_id,
+        },
+     }
+     ).then((updatedClient)=>{
+        res.json(updatedClient);
+     }).catch((err)=>{
+        console.log(err);
+        res.json(err);
+     });
+    });
 
 //delete a client
-router.delete('/:id', (req, res) => {
-
-});
+router.delete('/:id',async (req, res) => {
+    try {
+        const clientData = await Client.destroy({
+          where: {
+            id: req.params.id
+          }
+        });
+    
+        if (!clientData) {
+          res.status(404).json({ message: 'No client found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(clientData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
 
 module.exports = router;
--- routes/api/invoice-routes.js
+++ routes/api/invoice-routes.js
@@ -3,28 +3,84 @@ const router = require('express').Router();
 const { Invoice } = require('../../models');
 
 //find all invoice from database
-router.get('/', (req, res) => {
+router.get('/',  async(req, res) => {
 
+    try {
+        const invoiceData = await Invoice.findAll();
+        res.status(200).json(invoiceData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 //find invoice by ref_num
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async(req, res) => {
+    try {
+        const invoiceData = await async.findByPk(req.params.id, {
+         
+          include: [ref_num]
+        });
+    
+        if (!invoiceData) {
+          res.status(404).json({ message: 'No Invoice found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(invoiceData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 //create a new invoice 
-router.post('/', (req, res) => {
-
+router.post('/', async(req, res) => {
+    try {
+        const invoiceData = await Invoice.create(req.body);
+        res.status(200).json(invoiceData);
+      } catch (err) {
+        res.status(400).json(err);
+      }
 });
 
 //update invoice details
 router.put('/:id', (req, res) => {
-
+    Invoice.update({
+       invoice_number:req.body.invoice_number,
+        amount:req.body.amount,
+        memo:req.body.memo,   
+        due_date:req.body.due_date, 
+     },
+     {
+        where:{
+            invoice_id:req.params.invoice_id,
+        },
+     }
+     ).then((updatedInvoice)=>{
+        res.json(updatedInvoice);
+     }).catch((err)=>{
+        console.log(err);
+        res.json(err);
+     });
 });
 
 //delete a invoice
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async (req, res) => {
+    try {
+        const invoiceData = await Invoice.destroy({
+          where: {
+            id: req.params.id
+          }
+        });
+    
+        if (!invoiceData) {
+          res.status(404).json({ message: 'No invoice found with this id!' });
+          return;
+        }
+    
+        res.status(200).json(invoiceData);
+      } catch (err) {
+        res.status(500).json(err);
+      }
 });
 
 module.exports = router;
