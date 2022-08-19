2a41ee49de734b82da24cd331ec80e178662dce8
Jesus Islas <jesusislas96@gmail.com>
2022-08-19 16:35:30 -0400

    change jawsdb env variable

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 require("dotenv").config();
-const Sequelize = require("sequelize");
 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(session(sess));
 
 const routes = require('./routes');
 //define PORT
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3001;
 
 // Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
